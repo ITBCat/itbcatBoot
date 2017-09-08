b054b82122b0ed027cab13109bbf89b5fea510d5
BrickCat <549595297@qq.com>
2017-09-08 09:32:21 +0800

    menu list

--- target/classes/static/js/customjs/custom-datatable.js
+++ target/classes/static/js/customjs/custom-datatable.js
@@ -152,7 +152,24 @@ $(document).ready(function () {
     var dtable = $('.table').DataTable({
         "pagingType": "full_numbers_icon",
         order: [1, 'desc'],
-          responsive: true
+        responsive: true,
+        searching:false,
+        bInfo:true,
+        bStateSave: true,
+        oLanguage: {
+            sLengthMenu: "每页显示 _MENU_ 条记录",
+            sZeroRecords: "抱歉， 没有找到",
+            sInfo: "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+            sInfoEmpty: "没有数据",
+            sInfoFiltered: "(从 _MAX_ 条数据中检索)",
+            oPaginate: {
+                sFirst: "首页",
+                sPrevious: "上一页",
+                sNext: "下一页",
+                sLast: "尾页"
+            },
+            sZeroRecords: "没有检索到数据"
+        }
     });
 });
 
