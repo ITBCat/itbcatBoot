f2e63fe1104c2dccbf281f052f3daa40e867a0af
BrickCat <549595297@qq.com>
2017-09-12 17:16:24 +0800

    login & register

--- src/main/java/cn/itbcat/boot/repository/MenuRepository.java
+++ src/main/java/cn/itbcat/boot/repository/MenuRepository.java
@@ -2,12 +2,14 @@ package cn.itbcat.boot.repository;
 
 import cn.itbcat.boot.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by 860117030 on 2017/9/6.
  */
-public interface MenuRepository extends JpaRepository<Menu,String> {
+@Transactional
+public interface MenuRepository extends JpaRepository<Menu,String> ,CrudRepository<Menu,String> {
 
 
 
