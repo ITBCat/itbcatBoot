54958b6297c397b062a56a12955d6b9d37c03375
BrickCat <549595297@qq.com>
2017-09-17 20:57:31 +0800

    role

--- src/main/resources/static/sql/boot.sql
+++ src/main/resources/static/sql/boot.sql
@@ -1,22 +1,24 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : xxl-job
-Source Server Version : 50628
-Source Host           : localhost:3306
-Source Database       : boot
+ Source Server         : brickcat
+ Source Server Type    : MySQL
+ Source Server Version : 50718
+ Source Host           : localhost
+ Source Database       : boot
 
-Target Server Type    : MYSQL
-Target Server Version : 50628
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50718
+ File Encoding         : utf-8
 
-Date: 2017-09-14 17:18:09
+ Date: 09/17/2017 20:47:40 PM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `sys_dept`
+--  Table structure for `sys_dept`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_dept`;
 CREATE TABLE `sys_dept` (
@@ -30,12 +32,14 @@ CREATE TABLE `sys_dept` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_dept
+--  Records of `sys_dept`
 -- ----------------------------
-INSERT INTO `sys_dept` VALUES ('1505377991704', null, '测试部门', '0', '0', '一级部门');
+BEGIN;
+INSERT INTO `sys_dept` VALUES ('1505377991704', null, '测试部门', '0', '1', '一级部门'), ('1505543395303', null, '测试部门2', '0', '1505377991704', '测试部门');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_menu`
+--  Table structure for `sys_menu`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu` (
@@ -53,39 +57,36 @@ CREATE TABLE `sys_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_menu
+--  Records of `sys_menu`
 -- ----------------------------
-INSERT INTO `sys_menu` VALUES ('1505357921200', '0', 'user', '用户管理', '0', '0', '一级菜单', 'admin:user:view', '1', '/user/user');
-INSERT INTO `sys_menu` VALUES ('1505358775626', null, 'list layout', '菜单管理', '2', '0', '一级菜单', 'admin:menu:view', '1', '/menu/menu');
-INSERT INTO `sys_menu` VALUES ('1505359347214', '0', '0', '添加菜单', '0', '1505358775626', '菜单管理', 'admin:menu:add', '2', '/menu/add-menu');
-INSERT INTO `sys_menu` VALUES ('1505367916901', '0', '', '删除菜单', '0', '1505358775626', '菜单管理', 'admin:menu:delete', '2', '/menu/delete/');
-INSERT INTO `sys_menu` VALUES ('1505368312688', '0', '', '修改菜单', '0', '1505358775626', '菜单管理', 'admin:menu:edit', '2', '/menu/edit/');
-INSERT INTO `sys_menu` VALUES ('1505370854118', '0', '', '添加用户', '0', '1505357921200', '用户管理', 'admin:user:add', '2', '/menu/add-user');
-INSERT INTO `sys_menu` VALUES ('1505371378596', '0', '', '角色管理', '0', '0', '一级菜单', 'admin:role:view', '1', '/role/role');
-INSERT INTO `sys_menu` VALUES ('1505373308958', '0', '', '部门管理', '0', '0', '一级菜单', 'admin:dept:view', '1', '/dept/dept');
-INSERT INTO `sys_menu` VALUES ('1505374984860', '0', '', '添加部门', '0', '1505373308958', '部门管理', 'admin:dept:add', '2', '/dept/add-dept');
+BEGIN;
+INSERT INTO `sys_menu` VALUES ('1505357921200', '0', 'user', '用户管理', '0', '0', '一级菜单', 'admin:user:view', '1', '/user/user'), ('1505358775626', null, 'list layout', '菜单管理', '2', '0', '一级菜单', 'admin:menu:view', '1', '/menu/menu'), ('1505359347214', '0', '0', '添加菜单', '0', '1505358775626', '菜单管理', 'admin:menu:add', '2', '/menu/add-menu'), ('1505367916901', '0', '', '删除菜单', '0', '1505358775626', '菜单管理', 'admin:menu:delete', '2', '/menu/delete/'), ('1505368312688', '0', '', '修改菜单', '0', '1505358775626', '菜单管理', 'admin:menu:edit', '2', '/menu/edit/'), ('1505370854118', '0', '', '添加用户', '0', '1505357921200', '用户管理', 'admin:user:add', '2', '/menu/add-user'), ('1505371378596', '0', '', '角色管理', '0', '0', '一级菜单', 'admin:role:view', '1', '/role/role'), ('1505373308958', '0', '', '部门管理', '0', '0', '一级菜单', 'admin:dept:view', '1', '/dept/dept'), ('1505374984860', '0', '', '添加部门', '0', '1505373308958', '部门管理', 'admin:dept:add', '2', '/dept/add-dept'), ('1505546299087', '0', '', '添加角色', '0', '1505371378596', '角色管理', 'admin:role:add', '2', '/role/add-role');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_role`
+--  Table structure for `sys_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE `sys_role` (
   `role_id` varchar(255) NOT NULL,
   `create_time` datetime DEFAULT NULL,
   `del_flag` varchar(255) DEFAULT NULL,
-  `dept_id` varchar(255) DEFAULT NULL,
+  `dept_id` varchar(255) NOT NULL,
   `dept_name` varchar(255) DEFAULT NULL,
   `remark` varchar(255) DEFAULT NULL,
-  `role_name` varchar(255) DEFAULT NULL,
+  `role_name` varchar(255) NOT NULL,
   PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_role
+--  Records of `sys_role`
 -- ----------------------------
+BEGIN;
+INSERT INTO `sys_role` VALUES ('1505627342252', null, null, '1505377991704', '测试部门', '', '测试角色');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_role_dept`
+--  Table structure for `sys_role_dept`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_dept`;
 CREATE TABLE `sys_role_dept` (
@@ -97,11 +98,14 @@ CREATE TABLE `sys_role_dept` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_role_dept
+--  Records of `sys_role_dept`
 -- ----------------------------
+BEGIN;
+INSERT INTO `sys_role_dept` VALUES ('1505627382728', null, '1505377991704', '1505627342252'), ('1505627382740', null, '1505543395303', '1505627342252');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_role_menu`
+--  Table structure for `sys_role_menu`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_menu`;
 CREATE TABLE `sys_role_menu` (
@@ -113,11 +117,14 @@ CREATE TABLE `sys_role_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_role_menu
+--  Records of `sys_role_menu`
 -- ----------------------------
+BEGIN;
+INSERT INTO `sys_role_menu` VALUES ('1505627354371', null, '1505357921200', '1505627342252'), ('1505627375005', null, '1505370854118', '1505627342252'), ('1505627375009', null, '1505358775626', '1505627342252'), ('1505627375012', null, '1505359347214', '1505627342252'), ('1505627375017', null, '1505367916901', '1505627342252'), ('1505627375022', null, '1505368312688', '1505627342252'), ('1505627375026', null, '1505371378596', '1505627342252'), ('1505627375031', null, '1505546299087', '1505627342252'), ('1505627375035', null, '1505373308958', '1505627342252'), ('1505627375041', null, '1505374984860', '1505627342252');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_user`
+--  Table structure for `sys_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
@@ -136,12 +143,14 @@ CREATE TABLE `sys_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_user
+--  Records of `sys_user`
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', null, null, null, null, null, '549595297@qq.com', null, 'b954990a308d50ce8b844602ab3c40dc0c03f32b85b3f441abb8946912894909', null, 'ITBC');
+BEGIN;
+INSERT INTO `sys_user` VALUES ('1', null, null, null, '1505377991704', null, '549595297@qq.com', null, 'b954990a308d50ce8b844602ab3c40dc0c03f32b85b3f441abb8946912894909', null, 'ITBC');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `sys_user_role`
+--  Table structure for `sys_user_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_role`;
 CREATE TABLE `sys_user_role` (
@@ -153,5 +162,10 @@ CREATE TABLE `sys_user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_user_role
+--  Records of `sys_user_role`
 -- ----------------------------
+BEGIN;
+INSERT INTO `sys_user_role` VALUES ('1', '1', '1505627342252', '1');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
